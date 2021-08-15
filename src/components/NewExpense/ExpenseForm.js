79f7ad7eac1870cb5392d6c35900c6d7f906631a
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-08-16 00:47:46 +0200

    update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,6 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
- 
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -68,6 +67,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel} >Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
